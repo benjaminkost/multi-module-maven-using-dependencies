235aa888c83c4be32e12ebc89e59f2782e003eea
Benjamin Kostka <mail@ben-kostka.de>
2024-12-17 14:08:45 +0100

    Initial commit

---
+++ src/main/java/com/example/multimodulemavenusingdependencies/MultiModuleMavenUsingDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodulemavenusingdependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleMavenUsingDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleMavenUsingDependenciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multimodulemavenusingdependencies/MultiModuleMavenUsingDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodulemavenusingdependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleMavenUsingDependenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
